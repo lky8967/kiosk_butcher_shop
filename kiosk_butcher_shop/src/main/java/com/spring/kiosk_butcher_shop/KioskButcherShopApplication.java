d8e5e4a631b57d336812cb753df334438f34e1bf
lky8967 <lky8967@gmail.com>
2022-09-30 18:11:55 +0900

    첫 커밋

---
+++ kiosk_butcher_shop/src/main/java/com/spring/kiosk_butcher_shop/KioskButcherShopApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kiosk_butcher_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskButcherShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskButcherShopApplication.class, args);
+	}
+
+}
