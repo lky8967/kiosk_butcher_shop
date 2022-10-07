0fa6f5bb1ff764df066688e58db62b8ea82cc699
lky8967 <lky8967@gmail.com>
2022-10-08 01:35:39 +0900

    orderresponseDto

--- kiosk_butcher_shop/src/main/java/com/spring/kiosk_butcher_shop/order/OrderResponseDto.java
+++ kiosk_butcher_shop/src/main/java/com/spring/kiosk_butcher_shop/order/OrderResponseDto.java
@@ -1,2 +1,17 @@
-package com.spring.kiosk_butcher_shop.order;public class OrderResponseDto {
+package com.spring.kiosk_butcher_shop.order;
+
+import com.spring.kiosk_butcher_shop.orderBasket.OrderBasket;
+import com.spring.kiosk_butcher_shop.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderResponseDto {
+    private Long id;
+    private User user;
+    private List<OrderBasket> orderBaskets = new ArrayList<>();
+    //할인 가격
+    private Long discountPrice;
+    // 총 가격
+    private Long totalPrice;
 }
